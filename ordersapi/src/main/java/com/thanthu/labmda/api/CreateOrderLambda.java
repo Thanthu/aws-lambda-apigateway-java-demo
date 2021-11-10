05109458e0df88803f3db8ade76367cafda1bef5
Thanthu <tnair@orthofx.com>
2021-11-10 12:20:45 +0530

    Update createOrder Lambda to save item to DynamoDB. Closes #7

--- ordersapi/src/main/java/com/thanthu/labmda/api/CreateOrderLambda.java
+++ ordersapi/src/main/java/com/thanthu/labmda/api/CreateOrderLambda.java
@@ -1,5 +1,9 @@
 package com.thanthu.labmda.api;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,13 +12,23 @@ import com.thanthu.labmda.api.dto.Order;
 public class CreateOrderLambda {
 
 	private ObjectMapper objectMapper;
+	private DynamoDB dynamoDB;
+	private Table ordersTable;
 
 	public CreateOrderLambda() {
 		objectMapper = new ObjectMapper();
+		dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+		ordersTable = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
 	}
 
 	public APIGatewayProxyResponseEvent createOrder(APIGatewayProxyRequestEvent request) throws Exception {
 		Order order = objectMapper.readValue(request.getBody(), Order.class);
+		
+		Item item = new Item().withPrimaryKey("id", order.getId())
+				.withString("itemName", order.getItemName())
+				.withInt("quantity", order.getQuantity());
+		ordersTable.putItem(item);
+		
 		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(order.getId().toString());
 	}
 
--- ordersapi/src/test/java/com/thanthu/labmda/api/CreateOrderLambdaTest.java
+++ ordersapi/src/test/java/com/thanthu/labmda/api/CreateOrderLambdaTest.java
@@ -8,18 +8,5 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
 public class CreateOrderLambdaTest {
 
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testCreateOrder() throws Exception {
-		CreateOrderLambda createOrderLambda = new CreateOrderLambda();
-		APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
-		request.setBody("{\r\n" + 
-				"    \"id\": 1,\r\n" + 
-				"    \"itemName\": \"Mac Book Pro\",\r\n" + 
-				"    \"quantity\": 2\r\n" + 
-				"}");
-		APIGatewayProxyResponseEvent order = createOrderLambda.createOrder(request);
-		Assert.assertEquals("1", order.getBody());
-	}
 
 }
