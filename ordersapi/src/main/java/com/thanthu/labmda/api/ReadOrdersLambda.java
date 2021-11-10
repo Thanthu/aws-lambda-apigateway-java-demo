8067e0e2a9fb56ec5e3813662c74aae25822d8dd
Thanthu <tnair@orthofx.com>
2021-11-10 12:41:36 +0530

    Update getOrders Lambda to read data from DynamoDB. Closes #8

--- ordersapi/src/main/java/com/thanthu/labmda/api/ReadOrdersLambda.java
+++ ordersapi/src/main/java/com/thanthu/labmda/api/ReadOrdersLambda.java
@@ -1,5 +1,12 @@
 package com.thanthu.labmda.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,14 +15,26 @@ import com.thanthu.labmda.api.dto.Order;
 public class ReadOrdersLambda {
 
 	private ObjectMapper objectMapper;
+	private AmazonDynamoDB amazonDynamoDB;
 
 	public ReadOrdersLambda() {
 		objectMapper = new ObjectMapper();
+		amazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
 	}
 
 	public APIGatewayProxyResponseEvent getOrders(APIGatewayProxyRequestEvent request) throws Exception {
-		Order order = new Order(1L, "Mac Book Pro", 2);
-		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(objectMapper.writeValueAsString(order));
+
+		ScanResult scanResult = amazonDynamoDB.scan(new ScanRequest().withTableName(System.getenv("ORDERS_TABLE")));
+		
+		List<Order> orders = scanResult
+				.getItems()
+				.stream()
+				.map(item -> new Order(Long.parseLong(item.get("id").getN()),
+						item.get("itemName").getS(), 
+						Integer.parseInt(item.get("quantity").getN())))
+				.collect(Collectors.toList());
+		
+		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(objectMapper.writeValueAsString(orders));
 	}
 
 }
--- ordersapi/src/test/java/com/thanthu/labmda/api/CreateOrderLambdaTest.java
+++ ordersapi/src/test/java/com/thanthu/labmda/api/CreateOrderLambdaTest.java
@@ -1,11 +1,5 @@
 package com.thanthu.labmda.api;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-
 public class CreateOrderLambdaTest {
 
 
